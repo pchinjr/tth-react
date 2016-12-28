34d15fd1c0e1c0ca5cbca53c73ff1b97bebe31c4
Paul Chin Jr <pchinjr@gmail.com>
2016-12-28 15:00:47 +0000

    finished first app component

--- app.jsx
+++ app.jsx
@@ -1,8 +1,8 @@
-function Application() {
+function Application(props) {
   return (
-    <div className="application">
+    <div className="scoreboard">
       <div className="header">
-        <h1>Scoreboard</h1>
+        <h1>{props.title}</h1>
       </div>
     
       <div className="players">
@@ -18,9 +18,29 @@ function Application() {
             </div>
           </div>
         </div>
+    
+        <div className="player">
+          <div className="player-name">
+            Andrew Chalkey
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
+Application.propTypes = {
+  title: React.PropTypes.string,
+};
+
+Application.defaultProps = {
+  title: "Scoreboard",
+}
 ReactDOM.render(<Application />, document.getElementById('container'));
